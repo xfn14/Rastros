17057143be3d9a7e32395c8742f969a71b6839cb
andreubita <andre.vaz1411@gmail.com>
2020-03-12 21:19:54 +0000

    interpretador ADDED

---
+++ src/interface/interpretador.h
@@ -0,0 +1,12 @@
+#ifndef SRC_INTERPRETADOR_H
+#define SRC_INTERPRETADOR_H
+
+#include <stdio.h>
+#include <string.h>
+#include "../dados/data.h"
+#include "tabuleiro.h"
+#include "../logica/jogada.h"
+
+int interpretador(ESTADO *e);
+
+#endif //SRC_INTERPRETADOR_H
--- src/main.c
+++ src/main.c
@@ -3,17 +3,17 @@
 #include "dados/estado.h"
 #include "interface/tabuleiro.h"
 #include "logica/jogada.h"
-
-#define BUF_SIZE 1024
+#include "interface/interpretador.h"
 
 int main() {
 
     ESTADO *estado = inicializar_estado();
+    interpretador(estado);
     //COORDENADA coord_peca = obter_coordenada_peca(estado);
     //printf("%d", coord_peca.coluna);
     //printf("%d", coord_peca.linha);
 
-    mostrar_tabuleiro(estado);
+    //mostrar_tabuleiro(estado);
 
     return 0;
 }
