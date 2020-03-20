0eb128bd7205ca20e37546b895e03a8e8cb68359
Benjamim Meleiro Rodrigues <62024299+ben446@users.noreply.github.com>
2020-03-20 15:52:42 +0000

    Update tabuleiro.h

--- src/interface/tabuleiro.h
+++ src/interface/tabuleiro.h
@@ -6,7 +6,7 @@
 const CASA tabuleiro_inicial[8][8];
 
 /**
-/brief Função que recebe um estado e imprime o tabuleiro para esse estado.
+\brief Função que recebe um estado e imprime o tabuleiro para esse estado.
 */
 void mostrar_tabuleiro(ESTADO *e);
 
