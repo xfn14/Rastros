a048331ebe106348f5f2166ea65dd180218fa43f
Benjamim Meleiro Rodrigues <62024299+ben446@users.noreply.github.com>
2020-03-22 16:05:06 +0000

    Update estado.c

--- src/dados/estado.c
+++ src/dados/estado.c
@@ -72,7 +72,6 @@ void setTabuleiro(ESTADO *e, CASA tab[8][8]){
 \brief Função para obter a posição da PECA
 percorrendo a matriz da tabuleiro.
  */
-
 COORDENADA obter_coordenada_peca(CASA tab[8][8]){
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
